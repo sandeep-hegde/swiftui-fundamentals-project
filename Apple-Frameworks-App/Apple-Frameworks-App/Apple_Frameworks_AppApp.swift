1cee2aa749a88fff30ef97b5cd7eac0ed6a0696a
Sandeep Hegde <sandeep_hegde@outlook.com>
2023-08-01 01:07:00 +0530

    Initial project setup

---
+++ Apple-Frameworks-App/Apple-Frameworks-App/Apple_Frameworks_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Frameworks_AppApp.swift
+//  Apple-Frameworks-App
+//
+//  Created by Sandeep Hegde on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Frameworks_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
