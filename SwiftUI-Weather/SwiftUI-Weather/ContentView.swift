e524efbc856f279c2b28e03fa6cb64c6c812b11a
Sandeep Hegde <sandeep_hegde@outlook.com>
2023-07-31 15:46:39 +0530

    Added sun filled cloud SF symbol & sample city temperature

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -12,12 +12,26 @@ struct ContentView: View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
+            
             VStack {
                 Text("Bengaluru, IN")
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
+                
+                VStack(spacing: 8) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("28°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
                 Spacer()
+                
             }
         }
     }
